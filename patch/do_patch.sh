27e1ec6fd4314d84c9d49aff0e9650a62c2e78fb
Peter Dave Hello <hsu@peterdavehello.org>
2017-05-16 22:06:53 +0800

    Add ansible patch

--- patch/do_patch.sh
+++ patch/do_patch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
+cp $HOME/cord-tools/patch/patch_ansible $HOME/opencord/build
 cp $HOME/cord-tools/patch/patch_Vagrantfile $HOME/opencord/build
 cp $HOME/cord-tools/patch/patch_config $HOME/opencord/build
 cp $HOME/cord-tools/patch/patch_platform $HOME/opencord/build/platform-install
@@ -8,6 +9,7 @@ cp $HOME/cord-tools/patch/patch_maas $HOME/opencord/build/maas
 cd $HOME/opencord/build/
 git apply patch_config
 git apply patch_Vagrantfile
+git apply patch_ansible
 
 cd $HOME/opencord/build/platform-install
 git apply patch_platform
